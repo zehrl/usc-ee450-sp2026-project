95e52053aead3b06289fd3bc0a468039181ac084
Logan Zehr <zehrl315@outlook.com>
2026-04-26 09:12:38 -0500

    Add prescription stubs/skeleton

--- src/prescription_server.cpp
+++ src/prescription_server.cpp
@@ -1,6 +1,82 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include "../include/common.h"
 
-int main() {
-    std::cout << "Prescription server is up and running." << std::endl;
-    return 0;
+class PrescriptionServer
+{
+public:
+   void boot();
+   void run();
+
+   struct Prescription
+   {
+      std::string doctorName, patientHash, treatment, frequency;
+      bool found = false;
+   };
+
+   void loadPrescriptions(const std::string &filepath = "data/prescriptions.txt");
+   void savePrescriptions(const std::string &filepath = "data/prescriptions.txt");
+   bool addPrescription(const std::string &doctor, const std::string &patientHash,
+                        const std::string &treatment, const std::string &frequency);
+   Prescription findPrescription(const std::string &patientHash);
+
+   std::vector<Prescription> prescriptions;  // public for testing
+
+private:
+   void handlePrescribe(Message &msg, sockaddr_in &from);
+   void handleViewPrescription(Message &msg, sockaddr_in &from);
+
+   int sockfd = -1;
+};
+
+void PrescriptionServer::boot()
+{
+   loadPrescriptions();
+   sockfd = makeUDPSocket(PORT_PRESC);
+   std::cout << "The Prescription Server is up and running." << std::endl;
 }
+
+void PrescriptionServer::run()
+{
+   Message msg{};
+   sockaddr_in from{};
+   while (true)
+   {
+      udpRecv(sockfd, msg, from);
+      // handleRequest dispatch will go here
+   }
+}
+
+void PrescriptionServer::loadPrescriptions(const std::string &filepath)
+{
+}
+
+void PrescriptionServer::savePrescriptions(const std::string &filepath)
+{
+}
+
+bool PrescriptionServer::addPrescription(const std::string &doctor, const std::string &patientHash,
+                                          const std::string &treatment, const std::string &frequency)
+{
+   return false;
+}
+
+PrescriptionServer::Prescription PrescriptionServer::findPrescription(const std::string &patientHash)
+{
+   return Prescription{};
+}
+
+void PrescriptionServer::handlePrescribe(Message &msg, sockaddr_in &from) {}
+void PrescriptionServer::handleViewPrescription(Message &msg, sockaddr_in &from) {}
+
+#ifndef DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+int main()
+{
+   PrescriptionServer s;
+   s.boot();
+   s.run();
+}
+#endif
